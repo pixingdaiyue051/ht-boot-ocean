a79d0259613b9a031ce94ec1bf4f8e0ed8fff6f3
pixingdaiyue051 <pixingdaiyue051@163.com>
2019-01-26 12:49:10 +0800

    沙雕操作，放开ocean-api文件夹的忽略设置

---
+++ ocean-api/src/main/java/com/tequeno/common/constants/DemoConstants.java
@@ -0,0 +1,40 @@
+package com.tequeno.common.constants;
+
+public class DemoConstants {
+    /**
+     * 当前页码
+     */
+    public final static String CURRENTPAGE = "currentPage";
+    /**
+     * 分页参数
+     */
+    public final static String LIMIT = "limit";
+    /**
+     * 默认分页的查询方法,返回dto
+     */
+    public static final String SELECTALLBYCONDITION = "selectAllByCondition";
+    /**
+     * 默认分页的查询方法,返回map
+     */
+    public static final String SELECTMAPBYCONDITION = "selectMapByCondition";
+    /**
+     * 自定义方法关键字(主要用于判断,表达式右边的值无所谓)
+     */
+    public static final String ANOTHERMETHOD = "anotherMethod";
+    /**
+     * 排序关键字
+     */
+    public static final String ORDERBY = "orderby";
+    /**
+     * modalMap中加载成功与否标志
+     */
+    public static final String SUCCESS = "success";
+    /**
+     * modalMap中加载失败时的提示词
+     */
+    public static final String ERRORMSG = "errorMsg";
+    /**
+     * modalMap中加载成功时的提示词
+     */
+    public static final String DATA = "data";
+}
